c79bde20c6ad975cfcc3fbf75d395f3a4ab507cd
Stefan Bittmann <s.bittmann@outlook.de>
2021-02-18 16:54:42 +0100

    Interpreter

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
- 
+import Database from "./lib/Database.js"
+
+let db = new Database();
+
+(async () => {
+    let id = await db.table("cases").push({test: "test"});
+    console.log("ID: ", id);
+
+    let values = await db.table("cases").get(id);
+    console.log("GOT:", values);
+
+    let r = await db.table("cases").find((row) => {
+        return row.test === 'test'
+    });
+
+    db.table("cases").remove(r._id);
+    console.log(r);
+
+    let r2 = await db.table("cases").filter((row) => {
+        return row.test === 'test' && row.test2 === 2
+    });
+
+    console.log(r2);
+
+})()
