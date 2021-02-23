9dc355a28de92215bccd93074531afb8143b5b54
Stefan Bittmann <s.bittmann@outlook.de>
2021-02-23 16:15:31 +0100

    remove uneeded stuff

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-import Database from "./lib/Database.js"
-import Perf from "./lib/PerformanceCounter.js"
-import faker from "faker"
+import Database from "./lib/Database.js";
+import Perf from "./lib/PerformanceCounter.js";
+import faker from "faker";
 
 (async () => {
     let db = await new Database("faker");
-    
+    db.backup.import();
     let persons = [];
-    Perf.active = true
+    Perf.active = true;
 
     await db.table("persons").ensureIndex("name");
-    await db.table("persons").ensureIndex('username');
-    await db.table("persons").ensureIndex('email');
+    await db.table("persons").ensureIndex("username");
+    await db.table("persons").ensureIndex("email");
 
-    for(let i = 0; i < 1; i++) {
-        persons.push(faker.helpers.userCard())
-    }
-    
-    for(let i = 0; i < persons.length; i++) {
-        await db.table("persons").push(persons[i])
+    for (let i = 0; i < 1; i++) {
+        persons.push(faker.helpers.userCard());
     }
 
-    
-    let r = await db.table("persons").find((row) => {
-        return row.website === 'sherwood.biz';
-    }, {
-        r: "test"
-    });
-    //console.log(r)
+    for (let i = 0; i < persons.length; i++) {
+        let w = await db.table("persons").push(persons[i]);
+        console.log(w);
+        console.log(persons[i]);
+    }
 
+    let val = "abel.net";
+    let r = await db.table("persons").find(
+        (row) => {
+            console.log(row);
+            return row.website === val;
+        },
+        {
+            val,
+        },
+    );
+    console.log(r);
+    /*
     let r2 = await db.table("persons").filter((row) => {
         return row.website === 'sherwood.biz';
     });
@@ -38,38 +44,45 @@ import faker from "faker"
     for await(let row of r3) {
         //console.log(row)
     }
-
+    //*/
 
     Perf.active = false;
 
     let d = Perf.data;
 
-    let modulesTable = {
-    }
-    
-    for(let [key, value] of Object.entries(d)) {
-        let sum = value.times.reduce((pv, cv) => pv + cv, 0) 
-        let newKey = key + "                               "
+    let modulesTable = {};
+
+    for (let [key, value] of Object.entries(d)) {
+        let sum = value.times.reduce((pv, cv) => pv + cv, 0);
+        let newKey = key + "                               ";
         modulesTable[newKey.substr(0, 31)] = {
             sum: sum,
             called: value.times.length,
             avg: sum / value.times.length,
-            min: value.times.reduce((min, v) => min <= v ? min : v, Infinity),
-            max: value.times.reduce((max, v) => max >= v ? max : v, -Infinity),
-        }
+            min: value.times.reduce((min, v) => (min <= v ? min : v), Infinity),
+            max: value.times.reduce(
+                (max, v) => (max >= v ? max : v),
+                -Infinity,
+            ),
+        };
         let subs = value.subs;
-        for(let [key, value] of Object.entries(subs)) {
-            let sum = value.times.reduce((pv, cv) => pv + cv, 0) 
+        for (let [key, value] of Object.entries(subs)) {
+            let sum = value.times.reduce((pv, cv) => pv + cv, 0);
             let newKey = "    " + key + "                           ";
             modulesTable[newKey.substr(0, 31)] = {
                 sum: sum,
                 called: value.times.length,
                 avg: sum / value.times.length,
-                min: value.times.reduce((min, v) => min <= v ? min : v, Infinity),
-                max: value.times.reduce((max, v) => max >= v ? max : v, -Infinity),
-            }
+                min: value.times.reduce(
+                    (min, v) => (min <= v ? min : v),
+                    Infinity,
+                ),
+                max: value.times.reduce(
+                    (max, v) => (max >= v ? max : v),
+                    -Infinity,
+                ),
+            };
         }
-        
     }
 
     /*let s = db.db.createReadStream();
@@ -79,7 +92,7 @@ import faker from "faker"
     })*/
 
     console.table(modulesTable);
-    
+
     //console.log(r)
 
     /*
@@ -103,7 +116,4 @@ import faker from "faker"
     });
 
     console.log(r2);*/
-
-    
-
-})()
+})();
