0b570cf9c8607bf1b43cc0f9bf0715bd885dfaa9
Stefan Bittmann <s.bittmann@outlook.de>
2021-02-24 09:32:09 +0100

    update backup test

--- test/Database.js
+++ test/Database.js
@@ -1,63 +1,66 @@
-import Database from "../lib/Database.js"
-import Table from "../lib/Table.js"
-import fs from "fs/promises"
-import { expect } from 'chai'
+import Database from "../lib/Database.js";
+import Table from "../lib/Table.js";
+import Backup from "../lib/Backup.js";
+import fs from "fs/promises";
+import { expect } from "chai";
 
-let dbname = "databaseTest"
-
-
+let dbname = "databaseTest";
 
 describe("Database", () => {
-    let db
+    let db;
     before(async () => {
         await fs.rmdir(`./storage/${dbname}`, {
-            recursive: true
+            recursive: true,
         });
         db = await new Database(dbname);
     });
     after(async () => {
         await fs.rmdir(`./storage/${dbname}`, {
-            recursive: true
+            recursive: true,
         });
     });
 
     describe("#constructor(databaseName)", () => {
-        
         it("should create database files", async () => {
             let dbDir = await fs.readdir(`./storage/${dbname}`);
             expect(dbDir).to.have.lengthOf.greaterThan(0);
         });
 
         it("should create 'database'.meta.json", async () => {
-            let data = await fs.readFile(`./storage/${dbname}.meta.json`, "utf8");
+            let data = await fs.readFile(
+                `./storage/${dbname}.meta.json`,
+                "utf8",
+            );
             expect(data).to.not.be.undefined;
-        })
+        });
     });
     describe(".table(name)", () => {
         it("should return Table class", async () => {
             let table = db.table("test");
-            expect(table).to.be.instanceOf(Table)
+            expect(table).to.be.instanceOf(Table);
         });
         it("should return Table class from cache", async () => {
             let table = db.table("test");
-            expect(table).to.be.instanceOf(Table)
+            expect(table).to.be.instanceOf(Table);
         });
     });
-    describe(".backup(fileName)", () => {
-        it("should create backup", async () => {
-            await db.backup("./backup.json");
-        })  
-    })
+    describe(".backup", () => {
+        it("should return Backup class", async () => {
+            let backup = db.backup;
+            expect(backup).to.be.instanceOf(Backup);
+        });
+    });
     describe(".delete()", () => {
         before(async () => {
             await db.delete();
         });
 
-        it("should delete database files", async () => { 
-            expect(fs.readdir.bind(null ,`./storage/${dbname}`)).to.throw
+        it("should delete database files", async () => {
+            expect(fs.readdir.bind(null, `./storage/${dbname}`)).to.throw;
         });
         it("should delete 'database'.meta.json", async () => {
-            expect(fs.readFile.bind(null ,`./storage/${dbname}.meta.json`)).to.throw
+            expect(fs.readFile.bind(null, `./storage/${dbname}.meta.json`)).to
+                .throw;
         });
     });
-})
+});
